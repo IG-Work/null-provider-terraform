917975c7f7fdd5d685a87b70460abed2985251c2
Yashi Chauhan <64953869+yashi-coder@users.noreply.github.com>
2025-03-08 18:26:45 +0530

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.3"
+    }
+  }
+}
+
+provider "null" {
+# we can add configurations here
+}
+
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command = "echo Hello from Null Provider!"
+  }
+}
