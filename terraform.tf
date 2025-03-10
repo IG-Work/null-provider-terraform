00cc80aa1edafa0e15e2a8297379b3ff72d7b64c
Yashi Chauhan <64953869+yashi-coder@users.noreply.github.com>
2025-03-10 11:36:56 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -11,10 +11,16 @@ provider "null" {
 # we can add configurations here
 }
 
+variable "instance_name" {
+  description = "The name of the instance"
+  type        = string
+  default     = "example-instance"
+}
+
 resource "null_resource" "example" {
   count = 3
 
   provisioner "local-exec" {
-    command = "echo Hello from Null Provider with count index ${count.index}"
+    command = "echo Hello from Null Provider with count index ${count.index} and Instance name is ${var.instance_name}"
   }
 }
